21d7cbae4b17c9b6102fab2cae25c39804968f44
Jonathan <jonathan.pannetier@gmail.com>
2019-01-26 16:11:25 +0100

    Add spinner

--- app/src/main/kotlin/io/smallant/wizard/ui/MainViewModel.kt
+++ app/src/main/kotlin/io/smallant/wizard/ui/MainViewModel.kt
@@ -16,23 +16,27 @@ class MainViewModel(application: Application) : BaseViewModel(application) {
     private val uiScope = CoroutineScope(Dispatchers.Main + viewModelJob)
 
     private val wizardRepository = WizardRepository(RemoteDataSource(APIManager().apiWizardService))
+
+    private val _spinner = MutableLiveData<Boolean>()
     private val _content: MutableLiveData<String> = MutableLiveData()
 
     val content: LiveData<String>
         get() = _content
 
+    val spinner: LiveData<Boolean>
+        get() = _spinner
+
     fun fetchData() {
         uiScope.launch {
-            _content.value = "Loading..."
-            val message: String
+            _spinner.value = true
             val result = withContext(Dispatchers.IO) {
                 wizardRepository.fetchWizards()
             }
-
-            message = when (result) {
+            val message = when (result) {
                 is Result.Success -> result.data[0].fullname
                 is Result.Error -> result.exception.message ?: "Error on fetching wizards"
             }
+            _spinner.value = false
             _content.value = message
             sendToast("Fetching wizards finished!")
         }
