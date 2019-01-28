328ff0e346c9c8d80026e370f0dcb530ad2f8484
Jonathan <jonathan.pannetier@gmail.com>
2019-01-28 18:00:17 +0100

    Display error

--- app/src/main/kotlin/io/smallant/wizard/ui/features/home/HomeViewModel.kt
+++ app/src/main/kotlin/io/smallant/wizard/ui/features/home/HomeViewModel.kt
@@ -6,12 +6,12 @@ import androidx.lifecycle.MutableLiveData
 import io.smallant.wizard.data.models.houses.HogwartsHouse
 import io.smallant.wizard.ui.base.BaseViewModel
 import kotlinx.coroutines.launch
-import java.io.IOException
 
 class HomeViewModel(application: Application) : BaseViewModel(application) {
 
-    private val _spinner = MutableLiveData<Boolean>()
     private val _houses: MutableLiveData<List<HogwartsHouse>> = MutableLiveData()
+    private val _spinner = MutableLiveData<Boolean>()
+    private val _error = MutableLiveData<String>()
 
     val houses: LiveData<List<HogwartsHouse>>
         get() = _houses
@@ -19,13 +19,18 @@ class HomeViewModel(application: Application) : BaseViewModel(application) {
     val spinner: LiveData<Boolean>
         get() = _spinner
 
+    val error: LiveData<String>
+        get() = _error
+
     fun loadHouses() {
         uiScope.launch {
             try {
+                _error.value = null
                 _spinner.value = true
                 _houses.value = wizardRepository.fetchHouses()
-            } catch (exception: IOException) {
+            } catch (exception: Exception) {
                 _houses.value = null
+                _error.value = exception.message
             } finally {
                 _spinner.value = false
             }
